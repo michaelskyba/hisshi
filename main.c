5a749a1f71373203322824c7606b3470db40307e
Michael Skyba <michaelskyba2@gmail.com>
2024-09-22 00:15:34 -0400

    Basic line execution using stdlib system()

--- main.c
+++ main.c
@@ -7,14 +7,28 @@ void panic(char *msg) {
 }
 
 int main(int argc, char **argv) {
-	if (argc < 2)
+	if (argc != 2)
 		panic("Usage: hsh script-file");
+
+	printf("%s: received file %s\n", argv[0], argv[1]);
 
 	FILE *script_file = fopen(argv[1], "r");
 
 	int chunk_size = 100;
 	char line[chunk_size];
 
-	while (fgets(line, chunk_size, script_file))
-		printf("%s", line);
+	while (fgets(line, chunk_size, script_file))  {
+		if (*line == '#') {
+			printf("Skipping comment: %s", line);
+			continue;
+		}
+
+		if (*line == '\n') {
+			printf("Skipping blank line\n");
+			continue;
+		}
+
+		printf("Executing %s", line);
+		system(line);
+	}
 }
