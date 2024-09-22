ebe0322124d722f9fd8fd8d67b1e593c46914cce
Michael Skyba <michaelskyba2@gmail.com>
2024-09-22 16:54:58 -0400

    Track PID We're still using system() which is just falling back on /bin/sh. We can't have hisshi actually rely on other shells, obviously

--- main.c
+++ main.c
@@ -1,34 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 void panic(char *msg) {
 	fprintf(stderr, "%s\n", msg);
 	exit(1);
 }
 
+void execute(char *cmd) {
+	if (*cmd == '#') {
+		printf("Skipping comment: %s", cmd);
+		return;
+	}
+
+	if (*cmd == '\n') {
+		printf("Skipping blank line\n");
+		return;
+	}
+
+	printf("Executing %s on PID %d\n", cmd, getpid());
+	system(cmd);
+}
+
+void run_script(FILE *script_file) {
+	int chunk_size = 100;
+	char line[chunk_size];
+
+	while (fgets(line, chunk_size, script_file))
+		execute(line);
+}
+
 int main(int argc, char **argv) {
 	if (argc != 2)
 		panic("Usage: hsh script-file");
 
+	printf("Starting on PID %d\n", getpid());
+
 	printf("%s: received file %s\n", argv[0], argv[1]);
-
 	FILE *script_file = fopen(argv[1], "r");
-
-	int chunk_size = 100;
-	char line[chunk_size];
-
-	while (fgets(line, chunk_size, script_file))  {
-		if (*line == '#') {
-			printf("Skipping comment: %s", line);
-			continue;
-		}
-
-		if (*line == '\n') {
-			printf("Skipping blank line\n");
-			continue;
-		}
-
-		printf("Executing %s", line);
-		system(line);
-	}
+	run_script(script_file);
 }
