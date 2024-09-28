0264a9d4b9d9f0c12f174c861453bab7485c393a
Michael Skyba <michaelskyba2@gmail.com>
2024-09-28 01:27:19 -0400

    Slight line parsing restructure, better comments

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <dirent.h>
 #include <errno.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- src/parser.c
+++ src/parser.c
@@ -1,3 +1,8 @@
+enum state_phase {
+	command_name,
+	command_arg,
+};
+
 struct parse_state {
 	// Command we're constructing
 	struct command *cmd;
@@ -5,6 +10,9 @@ struct parse_state {
 	// The segment of the command we're constructing, like the path or an
 	// argument
 	char *token;
+
+	// Whether we're reading a command name token or an argument
+	int phase;
 
 	// Line number
 	int ln;
@@ -21,6 +29,7 @@ struct parse_state *create_state() {
 	// TODO address tokens longer than a fixed chunk_size of 100
 	state->token = malloc(100);
 
+	state->phase = command_name;
 	state->ln = 1;
 	state->last_status = -1;
 
@@ -31,7 +40,7 @@ void parse_script(FILE *script_file) {
 	struct parse_state *state = create_state();
 	char *p = state->token;
 
-	while (1) {
+	while (true) {
 		*p = getc(script_file);
 
 		// Doesn't require any parsing because we declare that every file must
@@ -39,42 +48,46 @@ void parse_script(FILE *script_file) {
 		if (*p == EOF)
 			break;
 
-		// TODO support trailing comments
-		if (*p == '#' && p == state->token) {
-			printf("%d: Skipping comment\n", state->ln++);
-			while (getc(script_file) != '\n') ;
-			continue;
-		}
-
-		if (*p != ' ' && *p != '\n') {
+		if (*p != ' ' && *p != '\n' && *p != '#') {
 			p++;
 			continue;
 		}
 
-		int is_newline = *p == '\n';
+		bool end_command = *p == '\n';
+
+		// Delete the rest of the line for comments
+		if (*p == '#') {
+			while (getc(script_file) != '\n') ;
+			end_command = true;
+		}
+
 		*p = '\0';
 
 		// TODO support leading/trailing spaces
 		// "Leading whitespace is unsupported, sorry :("
 		// 1727479203: Actually maybe we can unironically say that
 
-		if (p == state->token && is_newline) {
+		if (p == state->token && state->phase == command_name && end_command) {
 			printf("%d: Skipping blank line\n", state->ln++);
+			printf("but token is %s\n", state->token);
 			continue;
 		}
-
-		if (!state->cmd->path)
-			state->cmd->path = get_bin_path(state->token);
 
 		// Even if token == command name, set $0 as convention
 		add_arg(state->cmd, state->token);
 
-		if (is_newline) {
+		if (state->phase == command_name) {
+			state->cmd->path = get_bin_path(state->token);
+			state->phase = command_arg;
+		}
+
+		if (end_command) {
 			printf("Previous exit status: %d\n", state->last_status);
 			state->last_status = execute(state->cmd);
 			printf("Received new exit status: %d\n", state->last_status);
 
 			state->ln++;
+			state->phase = command_name;
 		}
 
 		p = state->token;
