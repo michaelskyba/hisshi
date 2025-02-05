953a5d835e1087f283e736590ef08dcc1954cc33
Michael Skyba <michaelskyba2@gmail.com>
2025-02-04 20:11:24 -0500

    Attempt to read from init script locations if present

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "hash_table.h"
@@ -38,9 +39,39 @@ int main(int argc, char **argv) {
 	ParseState *parse_state = create_parse_state();
 	ShellState *shell_state = create_shell_state(NULL);
 
+	// Run initialization script if available
+	char init_path[1024];
+	FILE *init_file = NULL;
+	const char *xdg_config_home = getenv("XDG_CONFIG_HOME");
+	const char *home = getenv("HOME");
+
+	if (xdg_config_home) {
+		snprintf(init_path, sizeof(init_path), "%s/hisshi/init", xdg_config_home);
+		if (access(init_path, R_OK) == 0) {
+			init_file = fopen(init_path, "r");
+		}
+	}
+	if (!init_file && home) {
+		snprintf(init_path, sizeof(init_path), "%s/.config/hisshi/init", home);
+		if (access(init_path, R_OK) == 0) {
+			init_file = fopen(init_path, "r");
+		}
+	}
+	if (!init_file) {
+		snprintf(init_path, sizeof(init_path), "/etc/hisshi_init");
+		if (access(init_path, R_OK) == 0) {
+			init_file = fopen(init_path, "r");
+		}
+	}
+	if (init_file) {
+		debug("Running init script: %s\n", init_path);
+		parse_script(parse_state, shell_state, init_file);
+		fclose(init_file);
+	}
+
 	// 1 offset: We want the filename of the script to be $0, not the hsh binary
 	// This is how regular shells do it
-	set_cli_args(shell_state, argc-1, argv+1);
+	set_cli_args(shell_state, argc - 1, argv + 1);
 
 	FILE *script_file = fopen(argv[1], "r");
 	assert(script_file);
